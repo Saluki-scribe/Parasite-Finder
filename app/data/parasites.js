7820d73316972356992b911e8b7f91c46bacb928
saluki-scribe <tara@nelsonwriting.com>
2017-09-30 01:57:27 -0400

    Added all parasite objects.

---
+++ app/data/parasites.js
@@ -0,0 +1,83 @@
+var parasiteArray = [
+    {
+        "name": "Human Botfly",
+        "photo": "",
+        "scores": [
+            5,
+            5,
+            2,
+            1,
+            2,
+            3,
+            4,
+            3,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "T. gondii",
+        "photo": "",
+        "scores": [
+            3,
+            1,
+            5,
+            5,
+            1,
+            2,
+            4,
+            5,
+            5,
+            4
+
+        ]
+    },
+    {
+        "name": "Tapeworm",
+        "photo": "",
+        "scores": [
+            4,
+            2,
+            4,
+            1,
+            5,
+            5,
+            1,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Lancet Liver Fluke",
+        "photo": "",
+        "scores": [
+            2,
+            2,
+            1,
+            5,
+            1,
+            1,
+            5,
+            5,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Tongue-eating Louse",
+        "photo": "",
+        "scores": [
+            1,
+            4,
+            1,
+            3,
+            3,
+            1,
+            1,
+            1,
+            5,
+            5
+        ]
+    }
+]
