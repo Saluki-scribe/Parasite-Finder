ae3011ffebf359f59b058a1c610a60ddb4c9c0b5
saluki-scribe <tara@nelsonwriting.com>
2017-09-30 10:54:42 -0400

    Modified ../routing/apiRoutes.js and htmlRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,20 @@
 //Require objects from parasites.js
 
 var parasiteData = require("../app/data/parasites.js");
+var hostData = require("../app/data/hosts.js");
 
 module.exports = function(app) {
+
+//Send all parasite objects in parasiteData array to endpoint '/api/parasites'
 
     app.get("/api/parasites", function(req, res) {
         res.json(parasiteData);
     });
 
+//Send posted information from survey results to the hostData array
+
     app.post("/api/parasites", function(req, res) {
-        res.json();
+        res.json(hostData);
     });
 
 };
