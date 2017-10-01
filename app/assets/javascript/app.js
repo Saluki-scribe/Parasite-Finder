5ec8d4637480733cccb6b9d3beeb21fee4c55f99
saluki-scribe <tara@nelsonwriting.com>
2017-09-30 20:33:50 -0400

    Used express.static to serve static files in assets folder.

---
+++ app/assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    console.log("This will work... eventually");
+    
+    $("h1").css("background-color", "red");
+    
+    $("#info-div").append("This still woooorks!");
+    });
+
--- server.js
+++ server.js
@@ -7,6 +7,16 @@ var path = require("path");
 var app = express();
 var PORT = 8080;
 
+//Serve static files located in assets folder
+
+app.use(function(req, res, next) {
+
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
+//app.use(express.static('assets'));
+
 //Parse posted data
 
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -21,3 +31,5 @@ require("./app/routing/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+module.exports = app;
